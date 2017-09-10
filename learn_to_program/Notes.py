157222bc6c0df01ad3f271d500aa823c3491d9e7
David <iceterminal@gmail.com>
2017-09-10 18:33:28 -0500

    Only notes update

--- learn_to_program/Notes.py
+++ learn_to_program/Notes.py
@@ -743,3 +743,50 @@ def shift_left(L):
 	L[-1] = first_item # takes original L[0] & puts it to end 						 of list
 	print(L)
 shift_left(letters)
+
+=============================================
++++++++++++Parallel Lists & Strings++++++++++
+
+def sum_items(list1, list2):
+	'''(list of number, list of number) -> list of number
+	
+	Return a new list in which each item is the sum of the items at the corresponding postition of list1 and list2.
+
+	Precondition: len(list1) == len(list2)
+	
+	>>> sum_items([1, 2, 3], [2, 4, 2])
+	[3, 6, 5]
+	>>> sum_items([1, 2], [2, 4, 2])
+	[3, 6]  #1st list only has 2 indexes, so it will never see 3rd on list2
+	>>> sum_items([2, 4, 2], [1, 2])
+	This will raise an IndexError because there is no index of 2 on 2nd list
+	''' 
+
+	sum_list = []
+	for i in range(len(list1)):
+		sum_list.append(list1[i] + list2[i])
+	return sum_list
+
+def count_matches(s1, s2):
+	''' (str, str) -> int
+
+	Return the nubmer of positions in s1 that contain the same character at the corresponding position of s2.
+
+	Precondition: len(s1) == len(s2)
+
+	>>> count_matches('ate', ape')
+	2
+	>>> count_matches('head', 'hard')
+	2
+	'''
+
+	num_matches = 0
+	for i in range(len(s1)):
+		if s1[i] == s2[i]:
+			num_matches + 1
+	return num_matches
+
+
+=============================================
++++++++++++++++++Nested Lists++++++++++++++++
+
