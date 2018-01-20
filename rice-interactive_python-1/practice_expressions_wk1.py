7b64f4d232267694835c8707eb718071c459fbcb
David <iceterminal@gmail.com>
2018-01-20 09:56:33 -0600

    Practice Expressions WK1

--- rice-interactive_python-1/practice_expressions_wk1.py
+++ rice-interactive_python-1/practice_expressions_wk1.py
@@ -10,10 +10,11 @@ print (2 * 3.14 * 8)
 
 print (3.14 * 8**2)
 
-print 1000 * (1 + .01 * 7) ** 10
+print (1000 * (1 + .01 * 7) ** 10)
 
 print ('My name is ' + 'Joe ' + 'Warren.')
 
 print str('Joe Warren is 52 years old. ')
 
 print ((2 - 5)**2 + (2 - 6)**2) ** .5
+print ((x0 - y0)**2 + (x1 - y1)**2) ** 0.5
