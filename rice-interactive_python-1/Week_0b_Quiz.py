1ab071880e18f30889730f5ad959a7800eb1bf5a
David <iceterminal@gmail.com>
2018-01-22 19:49:59 -0600

    Update quiz

---
+++ rice-interactive_python-1/Week_0b_Quiz.py
@@ -0,0 +1,97 @@
+Week 0b Quiz
+Quiz 0
+
+1. Question 1
+Which of the following are syntactically correct strings?
+
+Try each of them in CodeSkulptor.
+
+	Hello
+	"Hello"
+	'Hello'
+	"Hello
+	"It's a beautiful day."
+
+2. Question 2
+To display a value in the console, what Python keyword do you use?
+
+Enter answer here
+
+
+3. Question 3
+In the following code, there is one line starting with #. What does this line mean to Python?
+
+tax_rate = 0.15
+income = 40000
+deduction = 10000
+# Calculate income taxes
+tax = (income - deduction) * tax_rate
+print tax
+
+This text is used as a file name for the code.
+This is a comment aimed at the human reader. Python ignores such comments.
+This is a syntax error.
+This text is printed on the console.
+
+
+4. Question 4
+Which of the following arithmetic expressions are syntactically correct?
+Try each of them in CodeSkulptor.
+
+5 * 3 (7 - 2)
+3 * ((2 - 9) + 4)) * (2 + (1 - 3))
+(8 + (1 + (2 * 4) - 3))
+(7 - 2) / (3 ** 2)
+5 - 1 - 3 - 7 - 0
+
+5. Question 5
+You would like to make it so that the variable ounces has the value 16, thus representing one pound. What simple Python statement will accomplish this?
+
+ounces := 16
+ounces == 16
+16 = ounces
+ounces = 16
+
+6. Question 6
+A gram is equal to 0.035274 ounces. Assume that the variable mass_in_ounces has a value representing a given mass in ounces. Which Python statement below uses the variable mass_in_ounces to compute an equivalent mass mass_in_grams expressed in grams?
+
+Think about it mathematically, but also test these expressions in CodeSkulptor3. If you are still confused, you might check out this student tutorial video by Kelly on unit conversions.
+
+mass_in_grams = mass_in_ounces * 0.035274
+mass_in_ounces = 0.035274 * mass_in_grams
+mass_in_grams = 0.035274 / mass_in_ounces
+mass_in_grams = mass_in_ounces / 0.035274
+
+7. Question 7
+Which of the following can be used as a variable name?
+Try using each in CodeSkulptor.
+
+MYnumber
+16ounces
+my-number
+number123
+
+8. Question 8
+Assume you have values in the variables x and y. Which statement(s) would result in x having the sum of the current values of x and y?
+Test your answer in CodeSkulptor.
+
+x = x + y
+y += x+y
+x = y + x
+y += x
+
+9. Question 9
+Python file names traditionally end in what characters after a period? Don't include the period.
+Enter answer here
+
+10. Question 10
+We encourage you to save your CodeSkulptor Python files where?
+
+In "the cloud" only
+In "the cloud" and on your computer
+Nowhere — CodeSkulptor automatically saves your files for you
+On your computer only
+
+
+
+
